51947beb3a97a90801456441a2c6253b07ca91fb
dyhan316 <dyhan0316@gmail.com>
2023-12-02 04:48:11 +0000

    added to haar

--- report/analysis.py
+++ report/analysis.py
@@ -6,35 +6,46 @@ import matplotlib.pyplot as plt
 
 
 class Visualization:
-    def __init__(self, transformer: transform.Transform, dwt=True) -> None:
+    def __init__(self, transformer: transform.Transform, dwt=True, fewer = True) -> None:
         self.transformer = transformer
         self.reconstructed_images = None
+        self.fewer = fewer
     
     def compress_by_ratio(self, dwt=True):
-        retention_percentages = {
-            "100%": 100,
+        fewer_retention_percentages = {
+            "100%": 0,
+            "50%": 50,
+            "10%": 90,
+            "1%": 99,
+            "0.1%": 99.9,
+            "0.05%": 99.95,
+            "0.01%": 99.99
+        }
+        more_retention_percentages = {
+            "100%": 0,
             "50%": 50,
             "10%": 90,
             "1%": 99,
             "0.1%": 99.9,
             "0.05%": 99.95,
             "0.01%": 99.99,
-            # "0.008%": 99.992,
-            # "0.006%": 99.994,
-            # "0.004%": 99.996,
-            # "0.002%": 99.998,
-            # "0.001%": 99.999,
-            # "0.0008%": 99.9992,
-            # "0.0006%": 99.9994,
-            # "0.0004%": 99.9996,
-            # "0.0002%": 99.9998,
-            # "0.0001%": 99.9999,
-            # "0.00008%": 99.99992,
-            # "0.00006%": 99.99994,
-            # "0.00004%": 99.99996,
-            # "0.00002%": 99.99998,
-            # "0.00001%": 99.99999,
+            "0.008%": 99.992,
+            "0.006%": 99.994,
+            "0.004%": 99.996,
+            "0.002%": 99.998,
+            "0.001%": 99.999,
+            "0.0008%": 99.9992,
+            "0.0006%": 99.9994,
+            "0.0004%": 99.9996,
+            "0.0002%": 99.9998,
+            "0.0001%": 99.9999,
+            "0.00008%": 99.99992,
+            "0.00006%": 99.99994,
+            "0.00004%": 99.99996,
+            "0.00002%": 99.99998,
+            "0.00001%": 99.99999,
         }
+        retention_percentages = fewer_retention_percentages if self.fewer else more_retention_percentages
         images = {"original": self.transformer.loader.image_grayscaled}
 
         for percentage, value in tqdm(retention_percentages.items()):
@@ -54,35 +65,42 @@ class Visualization:
         utils.plot_images(images, **kwargs)
     
     def compare_the_waffle_by_compression_ratio(self, **kwargs):
+        images_cropped = {}
         if self.reconstructed_images:
             images = self.reconstructed_images
         else:
             self.compress_by_ratio()
             images = self.reconstructed_images
         for k, img in images.items():
-            images[k] = img[550:1150,3600:4200]
-        utils.plot_images(images, **kwargs)
+            images_cropped[k] = img[550:1150,3600:4200]
+        utils.plot_images(images_cropped, **kwargs)
     
-    def plot_rmse_by_compression_ratio(self, **kwargs):
+    def plot_rmse_by_compression_ratio(self,plot_onto_same_graph = False, **kwargs):
         original = self.transformer.loader.image_grayscaled
-        if self.reconstructed_images:
+        if self.reconstructed_images : 
             images = self.reconstructed_images
         else:
             self.compress_by_ratio()
             images = self.reconstructed_images
-
         rmse_list = []
         x_values = []
         for percentage, img in images.items():
-            rmse_list.append(utils.calculate_rmse(original, img))
-            x_values.append(1-0.01*float(percentage.rstrip('%')))  # Remove the percentage sign 
+            if percentage == "original":
+                pass
+            else : 
+                rmse_list.append(utils.calculate_rmse(original, img))
+                x_values.append(0.01*float(percentage.rstrip('%')))  # Remove the percentage sign 
+        
         plt.plot(x_values, rmse_list, **kwargs)
-        plt.xlabel("Compression Percentage")
-        plt.ylabel("RMSE")
-        plt.title("Reconstruction Error Rate vs Compression Percentage")
-        plt.show()
+        if not plot_onto_same_graph:
+            plt.xlabel("Compression Rate")
+            plt.ylabel("RMSE")
+            plt.title("Reconstruction Error Rate vs Compression Rate")
+            plt.legend()
+            plt.xscale("log")
+            plt.show()
     
-    def plot_histogram(self, percentages_to_plot,xrange, yrange, **kwargs):
+    def plot_histogram(self, percentages_to_plot,xrange = [0,1], yrange = [0,300000], **kwargs):
         original = self.transformer.loader.image_grayscaled
         if self.reconstructed_images:
             images = self.reconstructed_images
@@ -97,7 +115,7 @@ class Visualization:
             pixel_vals = utils.flatten_rescale(images[percentqage])
             ax.hist(pixel_vals, bins=256, range=[0, 1], density=False, alpha=0.3, label=f"Reconstructed Image ({percentqage})")
         ax.set_title('Histogram Comparison')
-        ax.set_xlabel('Pixel Value (normalized)')
+        ax.set_xlabel('Pixel percentage')
         ax.set_ylabel('Frequency')
         ax.legend()
         plt.xlim(xrange[0], xrange[1])
